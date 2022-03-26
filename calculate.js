38702f2b36269759c243addaa318201e01cb4114
naybala <naybalaismylife.nbl@gmail.com>
2022-03-26 18:36:12 +0630

    percent does not still work

---
+++ calculate.js
@@ -0,0 +1,39 @@
+let displayBox = document.querySelector(".display");
+let operator = ["+", "-", "*", "/", "%", "."];
+
+function showDisplay(x) {
+
+    let current = displayBox.innerHTML;
+    let lastIndex = current[current.length - 1];
+
+    if (displayBox.innerHTML == 0) {
+        return displayBox.innerHTML = x;
+    } else if (operator.includes(x) && operator.includes(lastIndex)) {
+        console.log(current.substring(0, current.length - 1) + x);
+        return displayBox.innerHTML = `${current.substring(0, current.length - 1)}${x}`;
+    }
+    return displayBox.innerHTML += x;
+}
+
+function calculate() {
+    let current = displayBox.innerHTML;
+    try {
+        displayBox.innerHTML = eval(current);
+    } catch (e) {
+        alert("Your Input is wrong");
+    }
+}
+
+function allClear() {
+    displayBox.innerHTML = 0;
+}
+
+function clearLast() {
+    let current = displayBox.innerHTML;
+    if (current.length == 1) {
+        displayBox.innerHTML = 0;
+
+    } else {
+        displayBox.innerHTML = current.substring(0, current.length - 1);
+    }
+}
